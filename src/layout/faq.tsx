e330033a9c7e4f35901fee389dd3b20a9883307e
Raphael Vitor <raphaelvitorlopes@icloud.com>
2025-04-24 18:32:26 -0300

    feat: add FAQ section and component

---
+++ src/layout/faq.tsx
@@ -0,0 +1,10 @@
+import { FAQ } from "@/components/faq";
+
+export default function FAQSection() {
+  return (
+    <div className="w-screen bg-blue/15 px-50 py-24 flex flex-col gap-12">
+      <h2 className="text-center text-blue font-bold text-5xl">Perguntas Frequentes</h2>
+      <FAQ />
+    </div>
+  );
+}
