34155c149428af91f7c245aa2599ee13e9940b5e
Raphael Vitor <raphaelvitorlopes@icloud.com>
2025-04-24 18:33:40 -0300

    feat: add theme colors, animations, and layout components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
+import About from "@/layout/about"
+import Activities from "@/layout/activities"
+import Benefits from "@/layout/benefits"
+import Hero from "@/layout/hero"
+import Testimonials from "@/layout/testimonials"
+import FAQ from "@/layout/faq"
+import CallToAction from "@/layout/cta"
+import Footer from "@/layout/footer"
+
 export default function Home() {
   return (
     <>
-    <h1>Hello World!</h1>
+      <Hero />
+      <Benefits />
+      <Activities />
+      <About />
+      <Testimonials />
+      <FAQ />
+      <CallToAction />
+      <Footer />
     </>
-  );
+  )
 }
