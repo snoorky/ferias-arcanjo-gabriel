81efca6ddde00a482c48d965b763d86b8ea918e5
Raphael Vitor <raphaelvitorlopes@icloud.com>
2025-05-13 16:05:54 -0300

    refactor: remove video and add image background

--- src/_layout/about.tsx
+++ src/_layout/about.tsx
@@ -1,6 +1,5 @@
 import { AboutAnimation } from "@/app/animations";
 import { JustifiedGallery } from "@/_components/gallery";
-import { Video } from "@/_components/video";
 
 export default function About() {
   return (
@@ -9,9 +8,10 @@ export default function About() {
       id="about"
     >
       <div className="flex flex-col xl:flex-row gap-8">
-        <div className="video rounded-xl overflow-hidden w-full h-full">
-          <Video />
-        </div>
+        <div
+          className="w-full md:w-4/6 m-auto h-60 lg:h-80 bg-cover bg-center rounded-xl"
+          style={{ backgroundImage: "url('/colegio.jpg')" }}
+        ></div>
         <div className="space-y-2 xl:w-2/3 xl:space-y-4 xl:text-start">
           <h4 className="subtitle font-bold -mb-1 text-green">Colégio</h4>
           <h2 className="title text-blue">Arcanjo Gabriel</h2>
