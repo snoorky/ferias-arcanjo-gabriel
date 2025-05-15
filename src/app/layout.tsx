2cfeee54abdd59b4ada475193ad1188c9b3d4769
Raphael Vitor <raphaelvitorlopes@icloud.com>
2025-05-15 15:36:47 -0300

    fix: remove facebook tag and add a google tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -76,37 +76,31 @@ export default function RootLayout({
           `}
         </Script>
 
-        {/* Meta Pixel */}
-        <Script id="facebook-pixel" strategy="afterInteractive">
+        {/* Google Tag Manager */}
+        <Script id="gtm-script" strategy="afterInteractive">
           {`
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '531320856313425');
-            fbq('track', 'PageView');
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-M8MZ9MN7');
           `}
         </Script>
       </head>
       <body className="antialiased">
-        {children}
-        <SpeedInsights />
-
-        {/* Facebook noscript fallback */}
+        {/* Google Tag Manager noscript */}
         <div
           dangerouslySetInnerHTML={{
             __html: `
-        <noscript>
-          <img height="1" width="1" style="display:none"
-          src="https://www.facebook.com/tr?id=531320856313425&ev=PageView&noscript=1"/>
-        </noscript>
-      `,
+              <noscript>
+                <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M8MZ9MN7"
+                height="0" width="0" style="display:none;visibility:hidden"></iframe>
+              </noscript>
+            `,
           }}
         />
+        {children}
+        <SpeedInsights />
       </body>
     </html>
   );
